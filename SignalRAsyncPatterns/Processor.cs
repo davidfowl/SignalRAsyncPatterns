aa455c4919036b4be33de34c2dbdc9c364516c2f
David Fowler <davidfowl@gmail.com>
2013-10-24 22:32:33 -0700

    Initial commit.

---
+++ SignalRAsyncPatterns/Processor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRAsyncPatterns
+{
+    public class Processor : Hub
+    {
+        private HttpClient _client = new HttpClient();
+
+        private static string[] _urls = new string[]
+        {
+            "http://www.microsoft.com",
+            "http://www.google.com",
+            "http://www.bing.com",
+            "http://www.reddit.com",
+            "http://news.ycombinator.com",
+        };
+
+        public async Task<List<string>> GetAllData()
+        {
+            var results = new List<string>();
+            foreach (var url in _urls)
+            {
+                results.Add(await MakeRequest(url));
+            }
+            return results;
+        }
+
+        public async Task LoadDataOnDemand()
+        {
+            foreach (var url in _urls)
+            {
+                Clients.Caller.loadResponse(await MakeRequest(url));
+            }
+        }
+
+        public Task LoadDataOnDemandParallel()
+        {
+            var tasks = new List<Task>(_urls.Length);
+            
+            foreach (var url in _urls)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    Clients.Caller.loadResponse(await MakeRequest(url));
+                }));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+
+        private async Task<string> MakeRequest(string url)
+        {
+            try
+            {
+                using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2)))
+                {
+                    var response = await _client.GetAsync(url, cts.Token);
+                    return url + " -> " + response.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                return url + " -> " + ex.ToString();
+            }
+        }
+    }
+}
