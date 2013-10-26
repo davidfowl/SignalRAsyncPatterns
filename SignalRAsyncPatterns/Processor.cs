7e715f224d4fe8e265f129fcbf77b622d69d6b6e
DamianEdwards <damian@damianedwards.com>
2013-10-25 18:24:35 -0700

    Moar stuff

--- SignalRAsyncPatterns/Processor.cs
+++ SignalRAsyncPatterns/Processor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace SignalRAsyncPatterns
             "http://news.ycombinator.com",
         };
 
-        public async Task<List<string>> GetAllData()
+        public async Task<List<string>> GetAllDataSerial()
         {
             var results = new List<string>();
             foreach (var url in _urls)
@@ -41,7 +42,7 @@ namespace SignalRAsyncPatterns
             return Task.WhenAll(results);
         }
 
-        public async Task LoadDataOnDemand()
+        public async Task LoadDataOnDemandSerial()
         {
             foreach (var url in _urls)
             {
@@ -49,19 +50,78 @@ namespace SignalRAsyncPatterns
             }
         }
 
-        public Task LoadDataOnDemandParallel()
+        public Task LoadDataOnDemandParallelViaDispatch()
         {
             var tasks = new List<Task>(_urls.Length);
             
             foreach (var url in _urls)
             {
-                tasks.Add(Task.Run(async () =>
+                tasks.Add(Task.Run(async () => SendResponse(await MakeRequest(url))));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+
+        public async Task LoadDataOnDemandParallelRequestsWithSerialCallbacks()
+        {
+            var tasks = _urls.Select(url => MakeRequest(url))
+                             .ToList();
+
+            while (tasks.Count > 0)
+            {
+                Task<string> completeTask = await Task.WhenAny(tasks);
+
+                tasks.Remove(completeTask);
+
+                SendResponse(await completeTask);
+            }
+        }
+
+        public Task LoadDataOnDemandAllParallelWithParallelCallbacksViaTAP()
+        {
+            var tasks = new List<Task>(_urls.Length);
+            
+            foreach (var url in _urls)
+            {
+                tasks.Add(MakeRequest(url).ContinueWith(t =>
                 {
-                    Clients.Caller.loadResponse(await MakeRequest(url));
+                    // Need to handle exceptions here manually via t.Exception
+                    
+                    SendResponse(t.Result);
                 }));
             }
 
             return Task.WhenAll(tasks);
+        }
+
+        public Task LoadDataOnDemandAllParallelWithParallelCallbacksViaNoLinq()
+        {
+            var tasks = new List<Task>(_urls.Length);
+            Func<string, Task> go = async url => SendResponse(await MakeRequest(url));
+
+            foreach (var url in _urls)
+            {
+                tasks.Add(go(url));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+
+        public Task LoadDataOnDemandAllParallelWithParallelCallbacksViaLinq()
+        {
+            return Task.WhenAll(_urls.Select(async url => SendResponse(await MakeRequest(url))));
+        }
+        /// <summary>
+        /// Coming in a future version...
+        /// </summary>
+        public Task LoadDataOnDemandAllParallelWithParallelCallbacksViaLinq(IProgress<string> progress)
+        {
+            return Task.WhenAll(_urls.Select(async url => progress.Report(await MakeRequest(url))));
+        }
+
+        private void SendResponse(string result)
+        {
+            Clients.Caller.loadResponse(result);
         }
 
         private async Task<string> MakeRequest(string url)
